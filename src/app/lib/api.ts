66fe982d29706bb8efc45f2164a59a6eceeaebba
aanno <aanno@users.noreply.github.com>
2024-08-22 16:30:56 +0200

    server rendering of math with @djencks/asciidoctor-mathjax

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,12 +1,15 @@
 // lib/api.ts
 import Asciidoctor from 'asciidoctor'
+import { register } from '@djencks/asciidoctor-mathjax'
 
 const asciidoctor = Asciidoctor()
+const registry = register(asciidoctor.Extensions.create())
 
 export const ad2Html: (ad: string) => string = (ad: string) => {
   const html = asciidoctor.convert(ad, {
     safe: 'safe',
     standalone: false,
+    extension_registry: registry,
   })
   return html as string
 }
